11fc61ee3e3954b54c8e8afb109891778917f8e7
nijaskhan <nijaskhan93@gmail.com>
2023-09-04 16:28:58 +0530

    useDispatchCovered

--- src/features/cake/CakeView.js
+++ src/features/cake/CakeView.js
@@ -1,16 +1,24 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { ordered, restocked } from './cakeSlice';
 
 const CakeView = () => {
     const cakeCount = useSelector((state) => state.cake.numOfCakes);
+    const dispatch = useDispatch();
     return (
         <>
             <div>
+                <span style={{ fontWeight: 'lighter', color: 'grey' }}>Buy one cake and get an icecream free</span>
                 <h1>Number of Cakes - {cakeCount}</h1>
             </div>
             <div style={{ display: 'flex', justifyContent: 'center', textAlign: 'center' }}>
-                <button style={{marginRight: '3px'}}>Order cake</button>
-                <button>Restock cake</button>
+                <button
+                    style={{ marginRight: '3px' }}
+                    onClick={() => dispatch(ordered())}
+                >Order cake</button>
+                <button
+                    onClick={()=> dispatch(restocked(5))}
+                >Restock cake</button>
             </div>
         </>
     );
--- src/features/icecream/IcecreamView.js
+++ src/features/icecream/IcecreamView.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { ordered, restocked } from './icecreamSlice';
 
 const IcecreamView = () => {
     const icecreamCount = useSelector((state) => state.icecream.numOfIcecream);
+    const dispatch = useDispatch();
     return (
         <>
             <div>
                 <h1>Number of Ice-Creams - {icecreamCount}</h1>
             </div>
             <div style={{ display: 'flex', justifyContent: 'center', textAlign: 'center' }}>
-                <button style={{marginRight: '3px'}}>Order Ice-cream</button>
-                <button>Restock Ice-cream</button>
+                <button
+                    style={{ marginRight: '3px' }}
+                    onClick={() => dispatch(ordered())}
+                >Order Ice-cream</button>
+                <button
+                    onClick={() => dispatch(restocked(10))}
+                >Restock Ice-cream</button>
             </div>
         </>
     );
